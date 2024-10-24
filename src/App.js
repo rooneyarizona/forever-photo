5c6559710c5a14563bfdd95917bdad24010fbf52
rooneyarizona <sagaralastair@gmail.com>
2024-10-24 16:16:12 -0700

    File organization

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import PhotoItem from './components/photo/PhotoItem';
-import Layout from './components/ui/Layout';
-<<<<<<< HEAD
-import Hamburger from './components/ui/Hamburger';
-=======
-import Hamburger from './components/Hamburger';
->>>>>>> 7d87f3d (Initial Commit)
-import Timeline from './components/timeline/Timeline';
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import PhotoItem from "./components/photo/PhotoItem";
+import Layout from "./components/ui/Layout";
+import Hamburger from "./components/ui/Hamburger";
+import Timeline from "./components/timeline/Timeline";
 
 function App() {
   return (
-    <BrowserRouter >
-    <Routes>
-    <Route path="/" element={<Layout />}>
-    <Route index element={<PhotoItem />} />
-    <Route path="photoItem" element={<PhotoItem />} />
-    <Route path="hamburger" element={<Hamburger />}/>
-    <Route path="timeline" element={<Timeline />}/>
-    </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<PhotoItem />} />
+          <Route path="photoItem" element={<PhotoItem />} />
+          <Route path="hamburger" element={<Hamburger />} />
+          <Route path="timeline" element={<Timeline />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
